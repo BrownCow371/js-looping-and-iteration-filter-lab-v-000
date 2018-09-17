351641b10ac8045e895a6eaa07e7c72c2b6dc0c9
Jamie Brown <jmbrowncow@gmail.com>
2018-09-17 20:13:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 function findMatching(array, name) {
   return array.filter(aValue => aValue.toLowerCase() === name.toLowerCase());
 }
+
+function fuzzyMatch(array, letters) {
+  return array.filter(aValue => aValue.startsWith(letters))
+}
